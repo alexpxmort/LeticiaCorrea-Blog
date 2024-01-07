2fd1096ceef90595c863d54f5aa797f74db47d27
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-07 12:59:56 -0300

    Update RecipeCard.tsx

--- src/ui/components/RecipeCard/RecipeCard.tsx
+++ src/ui/components/RecipeCard/RecipeCard.tsx
@@ -1,21 +1,27 @@
 // components/RecipeCard.js
-import { Box, Heading, Text, Stack, Image, Link } from '@chakra-ui/react';
+import { Box, Heading, Text, Link, VStack, Image } from '@chakra-ui/react';
 
 const RecipeCard = ({ recipe }) => {
   const { id, title, image, description } = recipe;
 
   return (
     <Link href={`/receitas/${id}`} _hover={{ textDecoration: 'none' }}>
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="md" mb={8} mx="auto">
+      <Box
+        maxW="sm"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        boxShadow="md"
+        mb={8}
+        mx="auto"
+      >
         <Image src={image} alt={title} objectFit="cover" boxSize="full" />
-        <Box p={4}>
-          <Heading as="h2" size="md" pt='2'>
+        <VStack p={4} align="start">
+          <Heading as="h2" size="md">
             {title}
           </Heading>
-          <Text fontSize='sm'>
-            {description}
-          </Text>
-        </Box>
+          <Text fontSize="sm">{description}</Text>
+        </VStack>
       </Box>
     </Link>
   );
