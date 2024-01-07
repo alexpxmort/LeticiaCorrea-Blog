8b4150b2f1ef0af1074c85956dd7bfd4232726fe
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-07 13:18:48 -0300

    Update RecipeCard.tsx

--- src/ui/components/RecipeCard/RecipeCard.tsx
+++ src/ui/components/RecipeCard/RecipeCard.tsx
@@ -3,8 +3,8 @@ const RecipeCard = ({ recipe }) => {
   const { id, title, image, description } = recipe;
 
   return (
-    <a href={`/receitas/${id}`} className="hover:no-underline">
-      <div className="w-full max-w-screen-sm border-1 border-gray-300 rounded-lg overflow-hidden shadow-md mb-8 mx-auto">
+    <a href={`/receitas/${id}`} className="w-full max-w-screen-sm hover:no-underline">
+      <div className=" border-1 border-gray-300 rounded-lg overflow-hidden shadow-md mb-8 mx-auto">
         <img src={image} alt={title} className="object-cover w-full h-48 md:h-64 lg:h-64 xl:h-64" />
         <div className="p-4">
           <h2 className="text-lg font-medium">{title}</h2>
