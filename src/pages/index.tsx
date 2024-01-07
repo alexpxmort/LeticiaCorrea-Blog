d690f854b6991ab78b87b57f57692d9e4a8ad0ff
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-07 13:27:06 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
 
       {/* Set padding on the body element for mobile spacing */}
       <div className="p-4">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4">
           {recipes.map(recipe => (
             <RecipeCard key={recipe.id} recipe={recipe} />
           ))}
