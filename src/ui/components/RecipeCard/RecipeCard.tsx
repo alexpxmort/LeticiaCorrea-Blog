de741d87aaf2c68ad0e98450520f4f8c3bd4f831
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-07 12:35:56 -0300

    Update RecipeCard.tsx

--- src/ui/components/RecipeCard/RecipeCard.tsx
+++ src/ui/components/RecipeCard/RecipeCard.tsx
@@ -1,29 +1,30 @@
 // components/RecipeCard.js
-import { Card, CardHeader, CardBody, Stack, Box, Heading, Text, StackDivider, Image } from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, Stack, Box, Heading, Text, StackDivider, Image, Link } from '@chakra-ui/react';
 
 const RecipeCard = ({ recipe }) => {
   const { id, title, image, description } = recipe;
 
   return (
-    <Card maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="md" mb={8} mx="auto">
-      <CardHeader>
-        <Image src={image} alt={title} objectFit="cover" boxSize="full" />
-        
-      </CardHeader>
-      <CardBody>
-        <Stack divider={<StackDivider />} spacing='4'>
-       <Heading size='md' pt='2'>
-          {title}
-        </Heading>
-          <Box>
-            <Text fontSize='sm'>
-              {description}
-            </Text>
-          </Box>
-          {/* Add more Box components as needed */}
-        </Stack>
-      </CardBody>
-    </Card>
+    <Link href={`/receitas/${id}`} _hover={{ textDecoration: 'none' }}>
+      <Card maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="md" mb={8} mx="auto">
+        <CardHeader>
+          <Image src={image} alt={title} objectFit="cover" boxSize="full" />
+          <Heading size='md' pt='2'>
+            {title}
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing='4'>
+            <Box>
+              <Text fontSize='sm'>
+                {description}
+              </Text>
+            </Box>
+            {/* Add more Box components as needed */}
+          </Stack>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
