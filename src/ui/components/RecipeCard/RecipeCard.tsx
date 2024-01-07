dd002375dacbb25edd463293f523eb71188705a5
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-07 12:11:56 -0300

    Update RecipeCard.tsx

--- src/ui/components/RecipeCard/RecipeCard.tsx
+++ src/ui/components/RecipeCard/RecipeCard.tsx
@@ -1,40 +1,22 @@
 // components/RecipeCard.js
-import { Box, Image, Heading, Text, Link } from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, CardFooter, Image, Text, Link, Stack, Heading } from '@chakra-ui/react';
 
 const RecipeCard = ({ recipe }) => {
   const { id, title, image, description } = recipe;
 
   return (
-    <Link href={`/receitas/${id}`}>
-      <Box
-        display="flex"
-        flexDirection="row" // Mudança para row
-        justifyContent="center"
-        alignItems="center"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        mb={8} // Ajuste o valor conforme necessário para aumentar o espaço vertical
-        boxShadow="md"
-        textAlign="center"
-        width="100%"
-        w="800px" // Ajuste a largura máxima conforme necessário
-        mx="auto" // Centraliza o card horizontalmente
-      >
-        <Image
-          src={image}
-          alt={title}
-          objectFit="cover"
-          boxSize="200px" // Ajuste o tamanho da imagem conforme necessário
-          mr={'48px'} // Adiciona um espaçamento à direita da imagem
-        />
-        <Box display={'flex'} flex={1} flexDirection={'column'} alignItems={'flex-start'} gap={3}>
-          <Heading as="h3" size="md" color={'#555'}>
+    <Link href={`/receitas/${id}`} _hover={{ textDecoration: 'none' }}>
+      <Card maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="md" mb={8} mx="auto">
+        <Image src={image} alt={title} objectFit="cover" boxSize="full" />
+        <Stack p={4}>
+          <Heading as="h2" size="md" color={'#555'}>
             {title}
           </Heading>
-          <Text color={'#333'}>{description}</Text>
-        </Box>
-      </Box>
+          <Text fontSize="md" color={'#333'}>
+            {description}
+          </Text>
+        </Stack>
+      </Card>
     </Link>
   );
 };
