4c5f564c77495a2449d27dbac32dbc7ff832e48e
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-07 22:43:48 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,29 @@ import RecipeCard from 'src/ui/components/RecipeCard/RecipeCard';
 import { Recipe } from 'src/domains/recipes/entities/Recipe'; // Adjust the path based on your project structure
 
 interface HomeProps {
-  recipes: Recipe[];
+  originalRecipes: Recipe[];
 }
 
-const Home: React.FC<HomeProps> = ({ recipes }) => {
-  const [allRecipes, setAllRecipes] = useState<Recipe[]>([]);
+const Home: React.FC<HomeProps> = ({ originalRecipes }) => {
+  const [additionalRecipes, setAdditionalRecipes] = useState<Recipe[]>([]);
+
+  const fetchAdditionalRecipes = async () => {
+    try {
+      // Fetch additional recipes from your API using recipeApi
+      const fetchedRecipes = await recipeApi.getAllRecipes();
+      setAdditionalRecipes(fetchedRecipes);
+    } catch (error) {
+      console.error('Error fetching additional recipes:', error);
+      // Handle error if necessary
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch additional recipes from your API using recipeApi
-        const additionalRecipes = await recipeApi.getAllRecipes();
-        setAllRecipes(additionalRecipes);
-      } catch (error) {
-        console.error('Error fetching additional recipes:', error);
-        // Handle error if necessary
-      }
-    };
-
-    // Call the fetchData function
-    fetchData();
+    // On initial load, you might want to fetch additional recipes immediately
+    fetchAdditionalRecipes();
   }, []); // Empty dependency array ensures the effect runs once after the initial render
 
-  const combinedRecipes = [...recipes, ...allRecipes];
+  const combinedRecipes = [...originalRecipes, ...additionalRecipes];
 
   return (
     <>
@@ -46,13 +46,27 @@ const Home: React.FC<HomeProps> = ({ recipes }) => {
           ))}
         </div>
       </div>
+
+      <button onClick={fetchAdditionalRecipes}>Reload Recipes</button>
     </>
   );
 };
 
 Home.defaultProps = {
-  recipes: [
+  originalRecipes: [
     // Initial recipes here
+    {
+      id: 'bolo-chocolate',
+      name: 'Bolo de Chocolate',
+      image: '/images/bolo-de-chocolate.jpeg',
+      description: 'Delicioso bolo de chocolate para satisfazer seus desejos.',
+    },
+    {
+      id: 'lasanha',
+      name: 'Lasanha',
+      image: '/images/lasanha.jpeg',
+      description: 'Deliciosa lasanha',
+    },
   ],
 };
 
