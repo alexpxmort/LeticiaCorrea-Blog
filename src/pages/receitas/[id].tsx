26528cab29b002b9dffd2a1fe6a7985e7344c8c4
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-13 17:21:57 -0300

    Update [id].tsx

--- src/pages/receitas/[id].tsx
+++ src/pages/receitas/[id].tsx
@@ -1,1 +1,42 @@
 
+/* eslint-disable max-len */
+// pages/recipes/bolo-de-chocolate.js
+import React, { useEffect, useState } from 'react';
+import { Recipe } from 'src/domains/recipes/entities/Recipe';
+import RecipePostPage from 'src/ui/components/RecipePostPage/RecipePostPage';
+import { recipeApi } from '@services/RecipeFetchApi';
+import { useRouter } from 'next/router';
+import LoadingSpinner from '@ui/components/Spinner';
+
+const Page = () => {
+const [loadingGet, setLoadingGet] = useState(true);
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const router = useRouter();
+
+  const getRecipe = async (id:string | string[] ) => {
+    const result = await recipeApi.getRecipe(id);
+    setRecipe(result);
+    setLoadingGet(false);
+  };
+  useEffect(() => {
+    if (router?.query?.id) {
+      getRecipe(router?.query?.id);
+    }
+  }, [router.query]);
+
+  if (loadingGet) {
+    return <LoadingSpinner />;
+  }
+  
+  return (
+    <RecipePostPage
+      sections={recipe?.sections ?? []}
+      recipe={{
+        title: recipe?.title,
+        image: recipe?.image 
+      }}
+    />
+  );
+};
+
+export default Page;
