c5ae82c787a8b29efba1507872fcde9cd3638bc6
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-13 17:08:36 -0300

    Update [linkName].tsx

--- src/pages/receitas/[linkName].tsx
+++ src/pages/receitas/[linkName].tsx
@@ -1,1 +1,19 @@
 
+/* eslint-disable max-len */
+// pages/recipes/bolo-de-chocolate.js
+import RecipePostPage from 'src/ui/components/RecipePostPage/RecipePostPage';
+
+
+const Page = () => {
+  return (
+    <RecipePostPage
+      sections={sections}
+      recipe={{
+        title: 'Bolo de Chocolate',
+        image: '/images/bolo-de-chocolate.jpeg'
+      }}
+    />
+  );
+};
+
+export default Page;
