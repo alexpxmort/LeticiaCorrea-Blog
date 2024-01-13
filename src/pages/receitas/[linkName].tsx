530579ede27e14e97cf3c25ae30a3442de0b5705
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-13 17:11:37 -0300

    Update [linkName].tsx

--- src/pages/receitas/[linkName].tsx
+++ src/pages/receitas/[linkName].tsx
@@ -1,6 +1,7 @@
 
 /* eslint-disable max-len */
 // pages/recipes/bolo-de-chocolate.js
+import React, { useEffect, useState } from 'react';
 import RecipePostPage from 'src/ui/components/RecipePostPage/RecipePostPage';
 import { recipeApi } from '@services/RecipeFetchApi';
 import { useRouter } from 'next/router';
