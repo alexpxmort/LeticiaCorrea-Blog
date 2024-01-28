a324cdcfb4eea3b0241dbf41ef7012002828bfe5
Alex <lexpdigi@gmail.com>
2024-01-28 19:05:37 -0300

    test: incrementa teste com cypress

--- cypress/integration/specs/test.spec.js
+++ cypress/integration/specs/test.spec.js
@@ -4,5 +4,9 @@ describe('Page /', () => {
     cy.visit('/');
     cy.contains('Letícia Correa Receitas').should('exist');
     cy.contains('2023 DexTI').should('exist');
+
+    cy.wait(1000);
+
+    cy.get('[data-testid="frango no leite de coco  e pimenta rosa"]').should('exist');
   });
 });
--- src/ui/components/RecipeCard/RecipeCard.tsx
+++ src/ui/components/RecipeCard/RecipeCard.tsx
@@ -1,9 +1,13 @@
 // components/RecipeCard.js
 const RecipeCard = ({ recipe }) => {
-  const { id, name, image, description,sections = []} = recipe;
+  const { id, name, image, description, sections = [] } = recipe;
 
   return (
-    <a href={`/receitas/${id}`} className="w-full max-w-screen-sm hover:no-underline">
+    <a
+      data-testid={`${name?.trim()?.toLowerCase()}`}
+      href={`/receitas/${id}`}
+      className="w-full max-w-screen-sm hover:no-underline"
+    >
       <div className=" border-1 border-gray-300 rounded-lg overflow-hidden shadow-md mb-8 mx-auto">
         <img src={image} alt={name} className="object-cover w-full h-48 md:h-64 lg:h-64 xl:h-64" />
         <div className="p-4">
@@ -15,10 +19,7 @@ const RecipeCard = ({ recipe }) => {
           </p>
         </div>
 
-        {
-!sections || sections?.length <=0 && <a href={`/receitas/${id}/sections/new`} >Completar cadastro</a>
-        }
-        
+        {!sections || (sections?.length <= 0 && <a href={`/receitas/${id}/sections/new`}>Completar cadastro</a>)}
       </div>
     </a>
   );
